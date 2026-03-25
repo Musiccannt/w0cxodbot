82fb7026a4cb1db4c5dea6cfbb1d9dcfffe49cb9
Musiccannt <mr.vinay@yandex.ru>
2026-03-26 03:00:24 +0500

    Первый коммит: ВосходБот

---
+++ songs.py
@@ -0,0 +1,20 @@
+# songs.py - список песен группы Восход
+
+PRESAVE_SONG = {
+    "title": "Музыка любви",
+    "url": "https://band.link/musiquedamour",
+    "is_presave": True,
+    "release_date": "8 апреля"
+}
+SONGS = [
+    {"title": "Альбом «Осознанные сны»", "url": "https://band.link/luciddreams"},
+    {"title": "Пустые глаза", "url": "https://band.link/mtyeyes"},
+    {"title": "Осенняя", "url": "https://band.link/osennya"},
+    {"title": "Словно ястреб", "url": "https://band.link/hawk"},
+    {"title": "Русская тоска", "url": "https://band.link/russkayatoska"},
+    {"title": "Я не сгорю", "url": "https://band.link/nesgoru"},
+    {"title": "Не приходи в моих снах", "url": "https://band.link/dontcomeinmydreams"},
+    {"title": "Ода", "url": "https://band.link/odetospace"},
+]
+
+ITEMS_PER_PAGE = 5
